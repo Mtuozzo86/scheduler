33995717c4114905e8ecc558a8fb7e692dd26c7c
Mtuozzo86 <mtuozzo86@gmail.com>
2023-02-18 11:22:58 -0500

    bringing selected time on button click to corresponding date back up to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,51 @@
-import './App.css';
-import { useState } from 'react'
-import Reserved from './Reserved';
+import "./App.css";
+import { useState } from "react";
+import Reserved from "./Reserved";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import WorkingDays from './WorkingDays';
+import WorkingDays from "./WorkingDays";
 
 function App() {
   const [clickedDay, onChange] = useState(new Date());
-  const [availableTimes, setAvailableTimes] = useState([])
-  const [availableDays, setAvailableDays] = useState([])
-  const [bookedTimes, setBookedTimes] = useState([])
-  console.log('state of days:', availableDays)
-
-  const hours = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm']
+  const [availableDays, setAvailableDays] = useState([]);
+  const hours = [
+    "9:00am",
+    "10:00am",
+    "11:00am",
+    "12:00pm",
+    "1:00pm",
+    "2:00pm",
+    "3:00pm",
+    "4:00pm",
+  ];
 
   function AddDayToAvailability(params) {
     if (availableDays.includes(params)) {
-      return
+      return;
     }
-    setAvailableDays([...availableDays, params])
-    
+    setAvailableDays([...availableDays, params]);
+  }
+
+  function handleSelectedTime(params, params2) {
+    console.log('hour selected from button click:', params, params2)
   }
 
   return (
     <div className="App">
-      <Calendar onChange={onChange} value={clickedDay} />
+      <Calendar calendarType="US" onChange={onChange} value={clickedDay} />
       <p>Selected day: </p>
       <p>{clickedDay.toDateString()}</p>
-      <button onClick={() => AddDayToAvailability(clickedDay.toDateString())}>Add</button>
-      <WorkingDays days={availableDays} />
+      <button onClick={() => AddDayToAvailability(clickedDay.toDateString())}>
+        Add
+      </button>
+      <WorkingDays days={availableDays} hours={hours} onAddTimeSlotToSelectedDay={handleSelectedTime} />
       <Reserved reserved={null} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- client/src/WorkingDays.js
+++ client/src/WorkingDays.js
@@ -1,16 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
-export default function WorkingDays({ days }) {
+export default function WorkingDays({ days, hours, onAddTimeSlotToSelectedDay }) {
+  const [selectedTime, setSelectedTime] = useState('')
+
+  function handleAddTimeSlotToSelectedDay(day) {
+    onAddTimeSlotToSelectedDay(selectedTime, day)
+  }
+
   const listOfDays = days.length > 0 && (
     <ul>
       {days.map((day) => (
-        <div style={{display: 'flex', margin: '0 200px'}}>
+        <div style={{ display: "flex", margin: "0 200px" }}>
           <li>{day}</li>
-          <button>times</button>
+          <select onChange={e => setSelectedTime(e.target.value)}>
+            <option value="All">Select a time</option>
+            {hours.map((hour) => (
+              <option value={hour}>{hour}</option>
+            ))}
+          </select>
+            <button onClick={() => handleAddTimeSlotToSelectedDay(day)}>
+              Add time to day
+            </button>
         </div>
       ))}
     </ul>
   );
+
   return (
     <div>
       <h2>My available days</h2>
